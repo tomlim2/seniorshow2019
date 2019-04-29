2fc325f4cda61bebf6ed86d46ff7852f5fd834fa
Younsoo Tom Lim <tomandlim@gmail.com>
2019-04-29 10:47:16 -0700

    Publish

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".designers").click(function(){
+    $( this ).toggleClass('designers-on');
+    // $(this).click(function(){
+    //   $(this).css({
+    //     'transform': 'translateY(' + 0 + 'px)'
+    //   });
+    // });
+  });
+});
