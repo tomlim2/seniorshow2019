8fe7b04d2109817781f6f0e2af7d35f99362c6b3
Younsoo Tom Lim <tomandlim@gmail.com>
2019-04-30 12:23:01 -0700

    randomColor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
+  var getRandomColors = function(){
+    var colors = ["#ec1750","#d5d829","#184a46","#6d1211","#ffc638","#faab20","#df4c9b","#000000","#9bd4be","#ffd500","#fff200","#b8d432","#f0525a","#506830","#8cc63f","#ef3842","#232021","#54c6f0","#f04e36","#0cb0d9","#9d85be","#cae9ea"];
+    var randomColors = Math.floor(Math.random() * 22);
+    return colors[randomColors];
+  };
 
   for (var i = 0; i < 22; i++) {
     var randomRotate = Math.random()*180;
-    var randomColor;
-    randomColor = Math.random() * 0x1000000; // 0 < randomColor < 0x1000000
-    randomColor = Math.floor(randomColor); // 0 < randomColor <= 0xFFFFFF
-    randomColor = randomColor.toString(16); // hex representation randomColor
-    randomColor = ("000000" + randomColor).slice(-6); // leading zeros added
+
     var randomLeft = Math.random()*1100;
     var randomTop = Math.random()*600;
     var drawing = $('<div class="drawing"></div>').css({
-        background: "#" + randomColor,
+        background: getRandomColors(),
         overflow: 'hidden',
         width: '160px',
-        height:'80px',
+        height:'60px',
         position: 'absolute',
         left: randomLeft,
         top: randomTop,
-        transform: 'rotate(' + randomRotate + 'deg)',
+        transform: 'rotate(' + randomRotate + 'deg)'
     });
     drawing.appendTo("#elements-container");
   }
